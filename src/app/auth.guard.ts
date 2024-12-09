f408de6235fee7fa60baaaab607a4b512dd68c61
Juliansv <juliansv22@gmail.com>
2024-12-09 13:34:08 +0100

    guard routes if user not logged in

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/router';
 import { AuthService } from './auth.service';
 import { inject } from '@angular/core';
-import { catchError, map, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 export const AuthGuard: CanActivateFn = (
   next: ActivatedRouteSnapshot,
@@ -20,17 +20,9 @@ export const AuthGuard: CanActivateFn = (
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  return authService.isLoggedIn().pipe(
-    map((loggedIn) => {
-      if (loggedIn) {
-        return true;
-      } else {
-        return router.createUrlTree(['/login']);
-      }
-    }),
-    catchError((error) => {
-      console.error('User not logged in', error);
-      return of(router.createUrlTree(['/login']));
-    })
-  );
+  if (!authService.isLoggedIn()) {
+    return of(router.createUrlTree(['/login']));
+  }
+
+  return authService.isLoggedIn();
 };
