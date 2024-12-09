6eda63899a0b2585981e665e11545bd32b40f73b
Juliansv <juliansv22@gmail.com>
2024-12-09 19:36:30 +0100

    update route to launch detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth.guard';
+import { LaunchDetailComponent } from './launch-detail/launchDetail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: 'launch/:id',
+    component: LaunchDetailComponent,
+    canActivate: [AuthGuard],
+  },
 ];
